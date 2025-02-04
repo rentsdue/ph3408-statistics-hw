ba73a0fa6fc60d2f6020c43e12b71fdd70ed19bf
Hogan Wong <hoganwong88@gmail.com>
2025-02-04 21:53:12 +0800

    wip

--- part5.py
+++ part5.py
@@ -3,4 +3,25 @@
 # the best fit you can get while fitting the data with a Poisson distribution? Below is how Luria
 # and Delbrück plot the data and compare it to the theoretical prediction in their original paper.
 # In your opinion, does the experimental data follow the expected distribution?
+import csv
 
+# Extract CSV file name
+filename1 = 'LDexpt22.csv'
+
+# Creating a list of values
+values = []
+
+# Reading through the CSV file
+with open(filename1, 'r') as csvfile:
+    # Creating a csv reader object
+    csvreader = csv.reader(csvfile)
+    
+    # Extracting field names through the first row
+    fields = next(csvreader)
+    
+    # Extracting and flattening each data row one by one, converting to float
+    for row in csvreader:
+        values.extend(float(value) for value in row)
+    
+# Mean is the lambda parameter of the Poisson Distribution
+mean = np.ave
